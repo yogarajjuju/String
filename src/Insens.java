4b2dd6515f0290470f83fef941769dc1c1c17a9a
Yogaraj <yogarajjuju22@gmail.com>
2025-06-21 16:46:39 +0530

    bankai

---
+++ src/Insens.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Insens {
+    public static void main(String[] args) {
+        String str = "heLLo";
+        str = str.toUpperCase();
+        System.out.println(str);
+        str=str.toLowerCase();
+        System.out.println(str);
+    }
+}
